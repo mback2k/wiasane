086ba1b433c785edf20163aab5a5d83882389e8b
Marc Hoersken <info@marc-hoersken.de>
2013-08-21 22:11:18 +0200

    Specified build target version

--- winsane-dbg/targetver.h
+++ winsane-dbg/targetver.h
@@ -1,8 +1,6 @@
 #pragma once
 
-// Durch Einbeziehen von"SDKDDKVer.h" wird die höchste verfügbare Windows-Plattform definiert.
+#include <WinSDKVer.h>
 
-// Wenn Sie die Anwendung für eine frühere Windows-Plattform erstellen möchten, schließen Sie "WinSDKVer.h" ein, und
-// legen Sie das _WIN32_WINNT-Makro auf die zu unterstützende Plattform fest, bevor Sie "SDKDDKVer.h" einschließen.
-
-#include <SDKDDKVer.h>
+#define WINVER 0x0601
+#define _WIN32_WINNT 0x0601
