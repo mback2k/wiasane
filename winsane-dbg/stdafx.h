1094d858f356037dcca217c0895863a4367daf4b
Marc Hoersken <info@marc-hoersken.de>
2013-08-22 13:35:04 +0200

    Improved header file structure

--- winsane-dbg/stdafx.h
+++ winsane-dbg/stdafx.h
@@ -1,4 +1,9 @@
+#ifndef STDAFX_H
+#define STDAFX_H
+
+#if _MSC_VER > 1000
 #pragma once
+#endif
 
 #include "targetver.h"
 
@@ -6,9 +11,13 @@
 #define WIN32_LEAN_AND_MEAN
 #endif
 
+#include <windows.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include <tchar.h>
 
 #include "winsane.h"
 
 #pragma comment(lib, "winsane.lib")
+
+#endif
--- winsane-dbg/targetver.h
+++ winsane-dbg/targetver.h
@@ -1,6 +1,13 @@
+#ifndef TARGETVER_H
+#define TARGETVER_H
+
+#if _MSC_VER > 1000
 #pragma once
+#endif
 
 #include <WinSDKVer.h>
 
 #define WINVER 0x0601
 #define _WIN32_WINNT 0x0601
+
+#endif
