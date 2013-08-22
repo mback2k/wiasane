a4baff22c80d84b5aee57c044677f9e48950f808
Marc Hoersken <info@marc-hoersken.de>
2013-08-22 19:19:07 +0200

    Auto-generated resources

--- winsane/resource.h
+++ winsane/resource.h
@@ -1,19 +1,17 @@
 //{{NO_DEPENDENCIES}}
 // Microsoft Visual C++ generated include file.
-// Used by wiasane.rc
+// Used by winsane.rc
 //
 #include <windows.h>
-
-#define IDS_SCAN_BUTTON_NAME            700
 
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        701
+#define _APS_NEXT_RESOURCE_VALUE        102
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_SYMED_VALUE           701
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
