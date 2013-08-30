c3b0bdfe9d071c66f3c476b46f323882f5d0faee
Marc Hoersken <info@marc-hoersken.de>
2013-08-30 22:05:12 +0200

    Added winsane library to wiasane microdriver

--- wiasane/stdafx.h
+++ wiasane/stdafx.h
@@ -11,6 +11,10 @@ _Analysis_mode_(_Analysis_code_type_user_driver_)
 #include <windows.h>
 #include <wiamicro.h>
 
+#include "winsane.h"
+
+#pragma comment(lib, "winsane.lib")
+
 extern HINSTANCE g_hInst; // instance of this MicroDriver (used for loading from a resource)
 
 #endif
