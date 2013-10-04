b3fc59236f77a930936650fe771c6e16b9a19bd6
Marc Hoersken <info@marc-hoersken.de>
2013-10-04 20:57:43 +0200

    Added CommonControls 6.0 requirement to manifest

--- coisane/dllmain.h
+++ coisane/dllmain.h
@@ -5,6 +5,8 @@
 #pragma once
 #endif
 
+#pragma comment(linker, "\"/manifestdependency:type='win32' name='Microsoft.Windows.Common-Controls' version='6.0.0.0' processorArchitecture='*' publicKeyToken='6595b64144ccf1df' language='*'\"")
+
 #include <DriverSpecs.h>
 __user_code
 
